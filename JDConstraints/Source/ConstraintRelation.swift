7edd3af952aac1c33200d41bb6837cbb14d34fa7
Joan Disho <dishojoan@gmail.com>
2018-08-13 13:33:39 +0200

    create ConstraintRelation enum

---
+++ JDConstraints/Source/ConstraintRelation.swift
@@ -0,0 +1,16 @@
+//
+//  ConstraintRelation.swift
+//  JDConstraints
+//
+//  Created by Joan Disho on 13.08.18.
+//  Copyright © 2018 Joan Disho. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public enum ConstraintRelation {
+    case equal
+    case equalOrLess
+    case equalOrGreater
+}
